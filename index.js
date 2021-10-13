e16fc7aa0400d92a1662b9fd0fb45f019b547302
aod1411 <nothingbutgood@comcast.net>
2021-10-13 12:53:00 -0400

    fixing

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 
 const OUT_DIR = path.resolve(__dirname, "dist")
-const outputPath = path.join(OUT_DIR, "team.html")
+const outputPath = path.join(OUT_DIR, "team.html");
+
+const render = require("./lib/htmlRenderer");
 
 const team = [];
 
